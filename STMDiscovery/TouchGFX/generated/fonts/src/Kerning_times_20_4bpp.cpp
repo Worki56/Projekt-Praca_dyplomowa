008913526a4f04c2a20129d6cb7e4eb71ef91e50
Worki56 <65239379+Worki56@users.noreply.github.com>
2023-02-20 00:49:02 +0100

    Add files via upload

---
+++ STMDiscovery/TouchGFX/generated/fonts/src/Kerning_times_20_4bpp.cpp
@@ -0,0 +1,15 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_times_20_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x0041, -1 },  // (First char = [0x0041, A], Second char = [0x0020,  ], Kerning dist = -1)
+    { 0x0020, -1 },  // (First char = [0x0020,  ], Second char = [0x0041, A], Kerning dist = -1)
+    { 0x0057, -2 },  // (First char = [0x0057, W], Second char = [0x0041, A], Kerning dist = -2)
+    { 0x0041, -1 },  // (First char = [0x0041, A], Second char = [0x0057, W], Kerning dist = -1)
+    { 0x0052, -1 },  // (First char = [0x0052, R], Second char = [0x0057, W], Kerning dist = -1)
+    { 0x0057, -1 },  // (First char = [0x0057, W], Second char = [0x0061, a], Kerning dist = -1)
+    { 0x0057, -1 },  // (First char = [0x0057, W], Second char = [0x0069, i], Kerning dist = -1)
+    { 0x0057, -1 },  // (First char = [0x0057, W], Second char = [0x006F, o], Kerning dist = -1)
+    { 0x0057, -1 },  // (First char = [0x0057, W], Second char = [0x0072, r], Kerning dist = -1)
+    { 0x0041, -1 },  // (First char = [0x0041, A], Second char = [0x0077, w], Kerning dist = -1)
+};
